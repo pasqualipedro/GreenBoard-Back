45fb17a7c5e2577550d74c27f72c9e8958fe4632
pasqualipedro <pasqualipedro@gmail.com>
2021-10-09 13:51:30 +0100

    Atualizando arquivo .env entre a dupla. Servidor e db rodando OK

--- config/db.config.js
+++ config/db.config.js
@@ -3,13 +3,9 @@
  */
 const mongoose = require(`mongoose`);
 
-const MONGO_PASS = `VYn69YEHQOKI`
-const DBNAME = `greenboardDB`;
-const MONGO_URI = `mongodb://admin:${MONGO_PASS}@cluster0-shard-00-00.mlab8.mongodb.net:27017,cluster0-shard-00-01.mlab8.mongodb.net:27017,cluster0-shard-00-02.mlab8.mongodb.net:27017/${DBNAME}?ssl=true&replicaSet=atlas-37km2k-shard-0&authSource=admin&retryWrites=true&w=majority`;
-
 const connect = async () => {
     try {
-        const connection = await mongoose.connect(MONGO_URI,
+        const connection = await mongoose.connect(process.env.MONGO_URI,
             {
                 useNewUrlParser: true,
                 useUnifiedTopology: true,
